fdc75c67096cec1fb54c020d08becf62a135af3a
Ekiosa <34338187+ekiosa@users.noreply.github.com>
2023-12-02 21:46:33 +0700

    Update pesan-makanan-online.js
    add cara Cara Pesan Makanan Online di Ayomakan

--- pages/pesan-makanan-online.js
+++ pages/pesan-makanan-online.js
@@ -11,10 +11,17 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Cara Pesan Makanan Online di Ayomakan" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Get started Cara Pesan Makanan Online di Ayomakan
         </p>
+        <h2>Cara Pesan Makanan Online di Ayomakan</h2>
+          <p>Anda dapat <a href="https://sidikul.com/pesan-makanan-online/" target="_blank">pesan makanan online</a> di Ayomakan melalui langkah-langkah berikut:</p>
+          <p>1. Buka website Ayomakan di browser Anda.</p>
+          <p>2. Cari dan pilih restoran atau menu favorit Anda.</p>
+          <p>3. Geser ke bawah dan klik "Pesan" untuk memilih makanan yang diinginkan.</p>
+          <p>Anda tidak perlu mengunduh aplikasi khusus, karena layanan pesan makanan online Ayomakan dapat diakses langsung melalui website mereka.&nbsp;</p>
+          <p>Dengan fitur-fitur yang mereka tawarkan, Anda dapat menikmati kemudahan dalam memesan makanan favorit Anda tanpa ribet.</p>
       </main>
 
       <Footer />
